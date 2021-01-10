0b16b492fe103ff48c4b5e49eb55b18cd81f2aa5
Joakim <joakim@dojo.dk>
2021-01-10 20:28:45 +0100

    Add docker

---
+++ build_and_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+mvn package -Dquarkus.package.type=uber-jar
+# Create a new docker image if necessary.
+# Restarts the container with the new image if necessary
+# The server stays running.
+# To terminate the server run docker-compose down in the
+# code-with-quarkus direcgtory
+docker-compose up -d
+# clean up images
+docker image prune -f 
+popd
+
+# Give the Web server a chance to finish start up
+sleep 2s
